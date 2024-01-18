ec1a13bab8ba5bd92824566a0459f3eafc8cb6ab
Magno Garofle <126581404+garoflemagno@users.noreply.github.com>
2024-01-18 18:19:26 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,32 @@
+let totalGeral;
+limpar();
+
+function adicionar() {
+    //função para recuperar os valores do nome do produto, quantidade e valor para implementação no código
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+
+    //calcular preço e adicionar no subtotal
+    let preco = quantidade * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos');
+    //adicionar no carrinho
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+        <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+        </section>`;
+
+    //atualizar o valor total
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$${totalGeral}`;
+    document.getElementById('quantidade').value = 0;
+
+}
+
+function limpar() {
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$0';
+
+}
